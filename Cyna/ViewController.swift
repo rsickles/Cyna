b34341c78780758442a523db0964b83e197fdeac
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-03-31 22:14:23 -0400

    Initial Commit

---
+++ Cyna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cyna
+//
+//  Created by Ryan Sickles on 3/31/15.
+//  Copyright (c) 2015 sickles.ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
