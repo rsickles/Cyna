1045ef9560dcff086e625fbaf6717dcc218dbab2
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-04-08 23:20:39 -0400

    Saving User Details To the Database with relation between user and account tables

---
+++ Cyna/CameraViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CameraViewController.swift
+//  Cyna
+//
+//  Created by Ryan Sickles on 4/4/15.
+//  Copyright (c) 2015 sickles.ryan. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Cyna/ViewController.swift
+++ Cyna/ViewController.swift
@@ -12,24 +12,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.viewDidLoad()
-        let testObject = PFObject(className: "TEST")
-        testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (result:Bool, error: NSError!) -> Void in
-            //
-        }
         if((FBSDKAccessToken.currentAccessToken()) != nil){
             //User is logged in, do work such as go to next view controller.
         }
-        
     }
     
     
     @IBAction func loginWithFacebook(sender: UIButton) {
         let login = FBSDKLoginManager.alloc()
         let permissions = ["email"]
-        login.logInWithReadPermissions(permissions, handler: { (result:FBSDKLoginManagerLoginResult!, error: NSError!) -> Void in
-            print(result)
+        login.logInWithReadPermissions(permissions, handler: { (result:FBSDKLoginManagerLoginResult!, error:NSError!) -> Void in
+        if((error) != nil) {
+            //Process error
+        } else if (result.isCancelled) {
+            // Handle cancellations
+        } else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+        if(result.grantedPermissions.containsObject("email")) {
+            //Grab User Data
+            if (FBSDKAccessToken.currentAccessToken() != nil) {
+                FBSDKGraphRequest(graphPath: "me", parameters: nil).startWithCompletionHandler({ (connection:FBSDKGraphRequestConnection!, result:AnyObject!, error:NSError!) -> Void in
+                    //saving user data
+                    var user = PFObject(className: "User")
+                    user["name"] = result.objectForKey("name")
+                    user["first_name"] = result.objectForKey("first_name")
+                    user["last_name"] = result.objectForKey("last_name")
+                    user["gender"] = result.objectForKey("gender")
+                    user["email"] = result.objectForKey("email")
+                    var userID = result.objectForKey("id") as NSString
+                    var facebookProfileUrl = "http://graph.facebook.com/\(userID)/picture?type=large"
+                    user["email"] = result.objectForKey("email")
+                    user["profile_picture"] = facebookProfileUrl
+                    var account = PFObject(className:"Account")
+                    account["account_active"] = true
+                    account["account_type"] = "user"
+                    user["parent"] = account
+                    user.saveInBackgroundWithBlock {
+                        (success: Bool, error: NSError!) -> Void in
+                        if (success) {
+                            // The object has been saved.
+                            //switch to home view controller
+                            print("Saved User!")
+                            //saving account data
+                        } else {
+                            // There was a problem, check error.description
+                            print("ERRROR")
+                        }
+                    }
+                })
+            }
+        }
+        }
         })
     }
 
